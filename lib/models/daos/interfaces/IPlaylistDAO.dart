1101783fd328e59694529e047622e1c98f8ff79a
LsrVictor <vl499690@iut-dijon.u-bourgogne.fr>
2025-10-01 18:25:03 +0200

    Début back (incomplet)

---
+++ lib/models/daos/interfaces/IPlaylistDAO.dart
@@ -0,0 +1,7 @@
+import 'package:mobile_app/models/dtos/playlistDTO.dart';
+import 'package:mobile_app/models/dtos/playlistsDTO.dart';
+
+abstract class IPlaylistDAO {
+  Future<PlaylistDTO?> getPlaylistById(String id);
+  Future<PlaylistsDTO?> getAllPlaylists();
+}
