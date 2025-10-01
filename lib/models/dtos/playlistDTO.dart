a9697b85f5dac13052ba40c3a42e2bcb5160d3db
LsrVictor <vl499690@iut-dijon.u-bourgogne.fr>
2025-10-01 18:25:03 +0200

    Début back (incomplet)

---
+++ lib/models/dtos/playlistDTO.dart
@@ -0,0 +1,13 @@
+class PlaylistDTO{
+  final String id;
+  final String name;
+  final String owner;
+  final String imageUrl;
+
+  PlaylistDTO({
+    required this.id,
+    required this.name,
+    required this.owner,
+    required this.imageUrl,
+  });
+}
