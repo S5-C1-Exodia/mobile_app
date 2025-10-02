e1e8fa4d04981840f329592c0c4ba2c28902b301
GuilhemBarriquand <barriquandguilhem@gmail.com>
2025-10-02 14:55:56 +0200

    Add missing files + rename files

--- lib/models/daos/interfaces/i_playlist_dao.dart
+++ lib/models/daos/interfaces/i_playlist_dao.dart
@@ -1,5 +1,5 @@
-import 'package:mobile_app/models/dtos/playlistDTO.dart';
-import 'package:mobile_app/models/dtos/playlistsDTO.dart';
+import 'package:mobile_app/models/dtos/playlist_dto.dart';
+import 'package:mobile_app/models/dtos/playlists_dto.dart';
 
 abstract class IPlaylistDAO {
   Future<PlaylistDTO?> getPlaylistById(String id);
