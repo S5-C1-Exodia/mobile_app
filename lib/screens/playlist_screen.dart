9d1dbb2518ac540a300981afd44298c5b21e9fea
Martin Lemoigne <martin.lemoigne@iut-dijon.u-bourgogne.fr>
2025-10-02 15:26:50 +0200

    Correction of the theme change

--- lib/screens/playlist_screen.dart
+++ lib/screens/playlist_screen.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../core/theme/palettes.dart';
 import '../models/playlist.dart';
+import '../providers/app_provider.dart';
 import '../widgets/playlists_list.dart';
 import '../widgets/custom_app_bar.dart';
 
 class PlaylistsScreen extends StatelessWidget {
-  final AppPalette palette;
   final VoidCallback onToggleTheme;
-  const PlaylistsScreen({super.key, required this.palette, required this.onToggleTheme});
+  final AppPalette palette;
+  const PlaylistsScreen({super.key, required this.onToggleTheme, required this.palette});
 
   @override
   Widget build(BuildContext context) {
+    final appProvider = Provider.of<AppProvider>(context);
+    final bool isDark = appProvider.themeMode == ThemeMode.dark;
+    final AppPalette palette = isDark ? paletteDark : paletteLight;
+
     final List<Playlist> samplePlaylists = [
       Playlist(name: 'Chill Vibes', autor: 'Damso', tracks: []),
       Playlist(name: 'Workout', autor: 'Ninho', tracks: []),
