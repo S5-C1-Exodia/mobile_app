488a91c6643d5748f3db5b9cc148052429a680e4
maxence teixeira <maxence.teixeira@iut-dijon.u-bourgogne.fr>
2025-10-10 10:48:11 +0200

    update logout button

--- lib/widgets/logout_button.dart
+++ lib/widgets/logout_button.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:mobile_app/viewmodels/connexion_vm.dart';
+import 'package:mobile_app/screens/splash_screen.dart';
+import '../providers/app_provider.dart';
+import '../core/theme/palettes.dart';
 
 class LogoutButton extends StatelessWidget {
   const LogoutButton({super.key});
@@ -8,6 +11,9 @@ class LogoutButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final connexionVM = Provider.of<ConnexionVM>(context, listen: false);
+    final appProvider = Provider.of<AppProvider>(context, listen: false);
+    final bool isDark = appProvider.themeMode == ThemeMode.dark;
+    final AppPalette palette = isDark ? paletteDark : paletteLight;
 
     return ElevatedButton.icon(
       icon: const Icon(Icons.logout),
@@ -18,6 +24,15 @@ class LogoutButton extends StatelessWidget {
       ),
       onPressed: () async {
         await connexionVM.disconnect();
+        if (context.mounted) {
+          Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+              builder: (_) =>
+                  SplashScreen(palette: palette, onToggleTheme: () {}),
+            ),
+            (route) => false,
+          );
+        }
       },
     );
   }
